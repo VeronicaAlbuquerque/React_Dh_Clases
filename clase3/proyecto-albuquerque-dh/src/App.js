dd8db997390d5053daa8e6d979b4df5c80f38b32
VeronicaAlbuquerque <valbuquerquedosreis@gmail.com>
2022-08-22 13:06:02 -0300

    carpetas react

---
+++ clase3/proyecto-albuquerque-dh/src/App.js
@@ -0,0 +1,20 @@
+import logo from './satisfied.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <br></br>
+          <h1>Una app limpia</h1>
+          <h2>Está muy feliz! :D</h2>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ clase5/entregamesa5/src/components/FunctionComponent.jsx
@@ -0,0 +1,13 @@
+
+    import React from 'react'
+
+    const FunctionComponent = (props) => {
+    return (
+    <li>{props.nombre} traerá {props.tarea}</li>
+    )
+    }
+    
+    export default FunctionComponent
+
+
+
